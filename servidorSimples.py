b1c683d2bf79557f2be90d99dcf6a691b1faf540
Antonio Roberto de Albuquerque Barros <robertobarros95@hotmail.com>
2019-10-03 12:00:42 -0300

    multi sockets
    agora pode obter mais de uma conexão ao mesmo tempo

--- servidorSimples.py
+++ servidorSimples.py
@@ -1,9 +1,206 @@
 import socket
+from threading import Thread
+import time
+
+
+class Sockts(Thread):
+    def __init__(self, addr, recurso):
+        Thread.__init__(self)
+        self.client_sock = recurso
+        self.addr = addr
+    def run(self):
+        while True:
+            try:
+                data = self.client_sock.recv(BUFSIZ)
+            except:
+                break
+            if not data or data.decode('utf-8') == 'END':
+                break
+            print("Received from client {}: {}".format(addr,data.decode('utf-8')))
+            try:
+                data = data.decode('utf-8')
+                comando = data.split(" ",1)
+                msg = funcoes(comando)
+                self.client_sock.send(msg.encode('utf-8'))
+            except KeyboardInterrupt:
+                print("Exited by user")
+        self.client_sock.close()
+        print(f"{addr} - Desconectado")
+
+class Consumidor(Thread):
+
+    def __init__(self, recurso, semaforo):
+        Thread.__init__(self)
+        self.recurso = recurso
+        self.semaforo = semaforo
+
+    def run(self):
+        for x in range(5):
+            self.semaforo.acquire()
+            i = self.recurso.pop()
+            print("Consumindo {}".format(i))
+            time.sleep(3)
+
 
 HOST ='localhost'
 PORT = 12346
 BUFSIZ = 4024
 ADDR = (HOST, PORT)
+
+def funcoes(comando):
+    if comando[0] == "CRIAR_ARQUIVO":
+        print("Estou criando um arquivo..")
+        nome_arquivo = comando[1]
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open('arquivos/lista.txt', 'r')
+            conteudo = arquivo.readlines()
+            jaExiste = False
+            for line in conteudo:
+                if nome_arquivo.lower() in line.lower():
+                    jaExiste = True
+            if not jaExiste:
+                conteudo.append(nome_arquivo + '\n')
+        except:
+            pass
+        arquivo = open('arquivos/lista.txt', 'w')
+        arquivo.writelines(conteudo)
+        arquivo.close()
+        arquivo = open(f"arquivos/{nome_arquivo}", 'w')
+        arquivo.close()
+        msg = f"Arquivo \"{nome_arquivo}\" criado com sucesso!"
+        print(msg)
+    elif comando[0] == "ESCREVER_ARQUIVO":
+        splitcomando = comando[1].split(" ", 1)
+        nome_arquivo = splitcomando[0]
+        texto = splitcomando[1]
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open(f"arquivos/{nome_arquivo}", 'r')
+            conteudo = arquivo.readlines()
+            if (nome_arquivo in conteudo) == False:
+                conteudo.append(texto + '\n')
+            arquivo = open(f"arquivos/{nome_arquivo}", 'w')
+            arquivo.writelines(conteudo)
+            arquivo.close()
+            msg = f"Arquivo \"{nome_arquivo}\" escrito com sucesso!"
+        except:
+            msg = f"O arquivo \"{nome_arquivo}\" não existe!"
+
+        print(msg)
+    elif comando[0] == "LISTAR":
+        try:
+            arquivo = open('arquivos/lista.txt', 'r')
+            conteudo = arquivo.readlines()
+            msg = "\nLista de Arquivos:\n"
+            for arq in conteudo:
+                msg += arq
+
+        except:
+            msg = f"Não existe arquivos criados!"
+        print(msg)
+    elif comando[0] == "MOSTRAR_LINHA-NOME_ARQUIVO":
+        splitcomando = comando[1].split(" ", 1)
+        nome_arquivo = splitcomando[0]
+        linha = int(splitcomando[1])
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open(f"arquivos/{nome_arquivo}", 'r')
+            conteudo = arquivo.readlines()
+            if len(conteudo) >= linha:
+                msg = f"Arquivo \"{nome_arquivo}\" - Conteudo da linha \'{linha}\': \"{conteudo[linha - 1]}\""
+            else:
+                msg = f"O arquivo \"{nome_arquivo}\" não tem essa linha!"
+        except:
+            msg = f"O arquivo \"{nome_arquivo}\" não existe!"
+
+        print(msg)
+    elif comando[0] == "BUSCAR_PALAVRA-NOME_ARQUIVO":
+        splitcomando = comando[1].split(" ", 1)
+        nome_arquivo = splitcomando[0]
+        palavra = splitcomando[1]
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open(f"arquivos/{nome_arquivo}", 'r')
+            conteudo = arquivo.readlines()
+            linhas = []
+            naotem = True
+            for i, linha in enumerate(conteudo, 1):
+                forma = ""
+                for x in range(0, len(linha)):
+                    if linha[x] != ' ' and linha[x] != '\n' and linha[x] != ',' and linha[x] != '.' and linha[
+                        x] != ':' and linha[x] != ';':
+                        forma += linha[x]
+                    else:
+                        if forma.lower() == palavra.lower():
+                            if not i in linhas:
+                                linhas.append(i)
+                            naotem = False
+                        forma = ""
+            if naotem:
+                msg = f"Arquivo \"{nome_arquivo}\" - Palavra não encontrada no arquivo."
+            else:
+                msg = f"A palavra \"{palavra}\" esta nas linhas {linhas} do arquivo \"{nome_arquivo}\"."
+        except Exception as e:
+            print(e)
+            msg = f"O arquivo \"{nome_arquivo}\" não existe!"
+
+        print(msg)
+    elif comando[0] == "CONTAR_LINHAS":
+        nome_arquivo = comando[1]
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open(f"arquivos/{nome_arquivo}", 'r')
+            conteudo = arquivo.readlines()
+
+            msg = f"O arquivo \"{nome_arquivo}\" tem \'{len(conteudo)}\' linhas"
+        except:
+            msg = f"O arquivo \"{nome_arquivo}\" não existe!"
+        print(msg)
+
+    elif comando[0] == "CONTAR_PALAVRAS":
+        nome_arquivo = comando[1]
+
+        if not ".txt" in nome_arquivo:
+            nome_arquivo = nome_arquivo + ".txt"
+        conteudo = ""
+        try:
+            arquivo = open(f"arquivos/{nome_arquivo}", 'r')
+            conteudo = arquivo.readlines()
+            palavras = 0
+            naotem = True
+            for i, linha in enumerate(conteudo, 1):
+                forma = ""
+                for x in range(0, len(linha)):
+                    if linha[x] != ' ' and linha[x] != '\n' and linha[x] != ',' and linha[x] != '.' and \
+                            linha[x] != ':' and linha[x] != ';':
+                        forma += linha[x]
+                    else:
+                        if forma.lower() != "":
+                            palavras += 1
+                            naotem = False
+                        forma = ""
+            if naotem:
+                msg = f"Arquivo \"{nome_arquivo}\" - não tem palavras."
+            else:
+                msg = f"O arquivo \"{nome_arquivo}\" tem \'{palavras}\' palavras."
+        except Exception as e:
+            print(e)
+            msg = f"O arquivo \"{nome_arquivo}\" não existe!"
+
+        print(msg)
+    return msg
+
 
 
 if __name__ == '__main__':
@@ -15,165 +212,23 @@ if __name__ == '__main__':
         print('Server waiting for connection...')
         client_sock, addr = server_socket.accept()
         print('Client connected from: ', addr)
-        while True:
-            data = client_sock.recv(BUFSIZ)
-            if not data or data.decode('utf-8') == 'END':
-                break
-            #print("Received from client: %s" % data.decode('utf-8'))
-            print("Received from client {}: {}".format(addr,data.decode('utf-8')))
-            try:
-                data = data.decode('utf-8')
-                comando = data.split(" ",1)
-
-                if comando[0] == "CRIAR_ARQUIVO":
-                    print("Estou criando um arquivo..")
-                    nome_arquivo = comando[1]
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open('arquivos/lista.txt', 'r')
-                        conteudo = arquivo.readlines()
-                        if (nome_arquivo in conteudo) == False:
-                            conteudo.append('\n' + nome_arquivo)
-                    except:
-                        pass
-                    arquivo = open('arquivos/lista.txt', 'w')
-                    arquivo.writelines(conteudo)
-                    arquivo.close()
-                    arquivo = open(f"arquivos/{nome_arquivo}", 'w')
-                    arquivo.close()
-                    msg = f"Arquivo \"{nome_arquivo}\" criado com sucesso!"
-                    print(msg)
-                elif comando[0] == "ESCREVER_ARQUIVO":
-                    splitcomando = comando[1].split(" ",1)
-                    nome_arquivo = splitcomando[0]
-                    texto = splitcomando[1]
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'r')
-                        conteudo = arquivo.readlines()
-                        if (nome_arquivo in conteudo) == False:
-                            conteudo.append(texto + '\n')
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'w')
-                        arquivo.writelines(conteudo)
-                        arquivo.close()
-                        msg = f"Arquivo \"{nome_arquivo}\" escrito com sucesso!"
-                    except:
-                        msg = f"O arquivo \"{nome_arquivo}\" não existe!"
-
-                    print(msg)
-                elif comando[0] == "LISTAR":
-                    try:
-                        arquivo = open('arquivos/lista.txt', 'r')
-                        conteudo = arquivo.readlines()
-                        msg = "\nLista de Arquivos:\n"
-                        for arq in conteudo:
-                            msg += arq
-
-                    except:
-                        msg = f"Não existe arquivos criados!"
-                    print(msg)
-                elif comando[0] == "MOSTRAR_LINHA-NOME_ARQUIVO":
-                    splitcomando = comando[1].split(" ", 1)
-                    nome_arquivo = splitcomando[0]
-                    linha = int(splitcomando[1])
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'r')
-                        conteudo = arquivo.readlines()
-                        if len(conteudo) >= linha:
-                            msg = f"Arquivo \"{nome_arquivo}\" - Conteudo da linha \'{linha}\': \"{conteudo[linha-1]}\""
-                        else:
-                            msg = f"O arquivo \"{nome_arquivo}\" não tem essa linha!"
-                    except:
-                        msg = f"O arquivo \"{nome_arquivo}\" não existe!"
-
-                    print(msg)
-                elif comando[0] == "BUSCAR_PALAVRA-NOME_ARQUIVO":
-                    splitcomando = comando[1].split(" ", 1)
-                    nome_arquivo = splitcomando[0]
-                    palavra = splitcomando[1]
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'r')
-                        conteudo = arquivo.readlines()
-                        linhas = []
-                        naotem = True
-                        for i,linha in enumerate(conteudo,1):
-                            forma = ""
-                            for x in range(0,len(linha)):
-                                if linha[x] != ' ' and linha[x] != '\n' and linha[x] != ',' and linha[x] != '.' and linha[x] != ':' and linha[x] != ';':
-                                    forma += linha[x]
-                                else:
-                                    if forma.lower() == palavra.lower():
-                                        if not i in linhas:
-                                            linhas.append(i)
-                                        naotem = False
-                                    forma = ""
-                        if naotem:
-                            msg = f"Arquivo \"{nome_arquivo}\" - Palavra não encontrada no arquivo."
-                        else:
-                            msg = f"A palavra \"{palavra}\" esta nas linhas {linhas} do arquivo \"{nome_arquivo}\"."
-                    except Exception as e:
-                        print(e)
-                        msg = f"O arquivo \"{nome_arquivo}\" não existe!"
-
-                    print(msg)
-                elif comando[0] == "CONTAR_LINHAS":
-                    nome_arquivo = comando[1]
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'r')
-                        conteudo = arquivo.readlines()
-
-                        msg = f"O arquivo \"{nome_arquivo}\" tem \'{len(conteudo)}\' linhas"
-                    except:
-                        msg = f"O arquivo \"{nome_arquivo}\" não existe!"
-                    print(msg)
-
-                elif comando[0] == "CONTAR_PALAVRAS":
-                    nome_arquivo = comando[1]
-
-                    if not ".txt" in nome_arquivo:
-                        nome_arquivo = nome_arquivo + ".txt"
-                    conteudo = ""
-                    try:
-                        arquivo = open(f"arquivos/{nome_arquivo}", 'r')
-                        conteudo = arquivo.readlines()
-                        palavras = 0
-                        naotem = True
-                        for i, linha in enumerate(conteudo, 1):
-                            forma = ""
-                            for x in range(0, len(linha)):
-                                if linha[x] != ' ' and linha[x] != '\n' and linha[x] != ',' and linha[x] != '.' and \
-                                        linha[x] != ':' and linha[x] != ';':
-                                    forma += linha[x]
-                                else:
-                                    if forma.lower() != "":
-                                        palavras += 1
-                                        naotem = False
-                                    forma = ""
-                        if naotem:
-                            msg = f"Arquivo \"{nome_arquivo}\" - não tem palavras."
-                        else:
-                            msg = f"O arquivo \"{nome_arquivo}\" tem \'{palavras}\' palavras."
-                    except Exception as e:
-                        print(e)
-                        msg = f"O arquivo \"{nome_arquivo}\" não existe!"
-
-                    print(msg)
-
-                client_sock.send(msg.encode('utf-8'))
-            except KeyboardInterrupt:
-                print("Exited by user")
-        client_sock.close()
+        '''         multiple socket         '''
+        recurso = client_sock
+        sockets = Sockts(addr,recurso)
+        sockets.start()
+        '''             Single sockt        '''
+        # while True:
+        #     data = client_sock.recv(BUFSIZ)
+        #     if not data or data.decode('utf-8') == 'END':
+        #         break
+        #     #print("Received from client: %s" % data.decode('utf-8'))
+        #     print("Received from client {}: {}".format(addr,data.decode('utf-8')))
+        #     try:
+        #         data = data.decode('utf-8')
+        #         comando = data.split(" ",1)
+        #         msg = funcoes(comando)
+        #         client_sock.send(msg.encode('utf-8'))
+        #     except KeyboardInterrupt:
+        #         print("Exited by user")
+        # client_sock.close()
     server_socket.close()
